2e9b510144b6844187d0f4c3d241615c560c8874
sangyx <sangyunxin@gmail.com>
2020-02-07 18:40:30 +0800

    test travis

---
+++ include/statistics/neighbors/kneighbors_classifier.hpp
@@ -0,0 +1,25 @@
+#ifndef MLKIT_KNEIGHBORS_CLASSIFIER_HPP
+#define MLKIT_KNEIGHBORS_CLASSIFIER_HPP
+namespace mk{
+    namespace neighbors{
+        class KNeighborsClassifier
+        {
+        private:
+            /* data */
+        public:
+            KNeighborsClassifier(/* args */);
+            ~KNeighborsClassifier();
+        };
+
+        KNeighborsClassifier::KNeighborsClassifier(/* args */)
+        {
+        }
+
+        KNeighborsClassifier::~KNeighborsClassifier()
+        {
+        }
+
+    }
+}
+
+#endif
