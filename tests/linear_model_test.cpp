cb425a74086307e654d8384731f2a36cf0f59867
sangyx <sangyunxin@gmail.com>
2020-02-11 09:51:26 +0800

    test

--- tests/linear_model_test.cpp
+++ tests/linear_model_test.cpp
@@ -22,14 +22,10 @@ TEST(LinearModelTestCase, LinearRegressionTest)
 TEST(LinearModelTestCase, LogisticRegressionTest)
 {
     array a = randu(10, 2);
-    array b = randu(2, 2);
 
     af_print(a);
+    array b = a.row(1);
     af_print(b);
-
-    a.row(3) = b.row(1);
-
-    af_print(a);
 
     array data = utils::load_dataset<float>("../tests/data/logistic_regression.txt");
     int feat = data.dims(1) - 1;
